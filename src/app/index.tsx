e1c9c5a41d0c7402e13c4d7a6a17d37c96c0a797
javel <javelawilson@gmail.com>
2024-11-26 17:13:51 -0600

    initial commit

---
+++ src/app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Page() {
+  return (
+    <View className="items-center justify-center flex-1">
+      <Text className="text-[64px] font-bold">Hinge Clone</Text>
+    </View>
+  );
+}
