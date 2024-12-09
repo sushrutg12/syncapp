e2b175663c172cb1056f0dbac57020ba889a8ab0
javel <javelawilson@gmail.com>
2024-12-09 18:41:32 -0500

    create auth stack layout

--- src/app/(app)/_layout.tsx
+++ src/app/(app)/_layout.tsx
@@ -1,6 +1,7 @@
-import { Stack } from "expo-router";
+import { Redirect, Stack } from "expo-router";
 
 export default function Layout() {
+  return <Redirect href={"/sign-in"} />;
   return (
     <Stack
       screenOptions={{
