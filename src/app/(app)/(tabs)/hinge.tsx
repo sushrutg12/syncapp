43a2bcb94db6ef3d0343f4c0e84e4016d8dcf1c5
javel <javelawilson@gmail.com>
2024-12-14 02:09:41 -0500

    implement hinge page ui

--- src/app/(app)/(tabs)/hinge.tsx
+++ src/app/(app)/(tabs)/hinge.tsx
@@ -1,9 +1,55 @@
-import { Text, View } from "react-native";
+import { useMyProfile } from "@/api/my-profile";
+import { Card } from "@/components/card";
+import { Ionicons } from "@expo/vector-icons";
+import { Image } from "expo-image";
+import { Stack } from "expo-router";
+import { Pressable, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import HingeLogo from "~/assets/images/hinge-logo.svg";
 
 export default function Page() {
+  const { data: profile } = useMyProfile();
   return (
-    <View>
-      <Text>Hinge</Text>
-    </View>
+    <SafeAreaView className="flex-1 bg-white">
+      <Stack.Screen
+        options={{
+          headerShown: false,
+        }}
+      />
+      <View className="px-5 border-b border-neutral-300">
+        <View className="flex-row items-center justify-between">
+          <HingeLogo width={64} />
+          <View className="flex-row items-center gap-4">
+            <Ionicons name="options-outline" className="text-2xl" />
+            <Ionicons name="settings-outline" className="text-2xl" />
+          </View>
+        </View>
+        <View className="items-center gap-2 my-12">
+          <Pressable className="h-32 aspect-square rounded-full border-4 border-fuchsia-900 p-1">
+            <Image
+              source={profile?.avatar_url}
+              className="flex-1 rounded-full bg-neutral-400"
+            />
+          </Pressable>
+          <Text className="text-2xl font-poppins-semibold">
+            {profile?.first_name}
+          </Text>
+        </View>
+      </View>
+      <View className="flex-1 p-5 gap-4">
+        <Card
+          key={"help"}
+          icon={<Ionicons name="help" className="text-2xl" />}
+          title="Help Center"
+          subtitle="Safety, Security, and more"
+        />
+        <Card
+          key={"what-works"}
+          icon={<Ionicons name="bulb-outline" className="text-2xl" />}
+          title="What Works"
+          subtitle="Check out our expert dating tips"
+        />
+      </View>
+    </SafeAreaView>
   );
 }
---
+++ src/components/card.tsx
@@ -0,0 +1,30 @@
+import React, { FC, ReactNode, forwardRef } from "react";
+import { Pressable, PressableProps, Text, View } from "react-native";
+
+interface Props extends PressableProps {
+  icon: ReactNode;
+  title: string;
+  subtitle: string;
+}
+
+export const Card: FC<Props> = forwardRef<View, Props>(
+  ({ icon, title, subtitle, ...rest }, ref) => {
+    return (
+      <Pressable
+        ref={ref}
+        {...rest}
+        className="flex-row items-center gap-4 border border-neutral-300 rounded-md p-3"
+      >
+        <View className="bg-zinc-300 h-12 aspect-square rounded-full items-center justify-center">
+          {icon}
+        </View>
+        <View>
+          <Text className="text-base font-poppins-medium">{title}</Text>
+          <Text className="text-sm font-poppins-light">{subtitle}</Text>
+        </View>
+      </Pressable>
+    );
+  }
+);
+
+Card.displayName = "Card";
