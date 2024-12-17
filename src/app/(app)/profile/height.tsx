80d8ee6d84cbe3114194e2f6a5660ff7fbdfaf54
javel <javelawilson@gmail.com>
2024-12-17 01:51:51 -0500

    implement height page

--- src/app/(app)/profile/height.tsx
+++ src/app/(app)/profile/height.tsx
@@ -1,9 +1,39 @@
-import { Text, View } from "react-native";
+import { PrivateProfile } from "@/api/my-profile/types";
+import { StackHeaderV4 } from "@/components/stack-header-v4";
+import { useEdit } from "@/store/edit";
+import { Picker } from "@react-native-picker/picker";
+import { router } from "expo-router";
+import { range } from "lodash";
+import { useState } from "react";
+import { View } from "react-native";
 
 export default function Page() {
+  const { edits, setEdits } = useEdit();
+  const [selectedHeight, setSelectedHeight] = useState(
+    edits?.height_cm || undefined
+  );
+
+  const handlePress = () => {
+    if (selectedHeight) {
+      setEdits({
+        ...edits,
+        height_cm: selectedHeight,
+      } as PrivateProfile);
+    }
+    router.back();
+  };
+
   return (
-    <View>
-      <Text>Height</Text>
+    <View className="flex-1 bg-white p-5">
+      <StackHeaderV4 title="Height" onPressBack={handlePress} />
+      <Picker
+        selectedValue={selectedHeight}
+        onValueChange={(itemValue, _itemIndex) => setSelectedHeight(itemValue)}
+      >
+        {range(92, 214).map((height) => (
+          <Picker.Item key={height} label={`${height} cm`} value={height} />
+        ))}
+      </Picker>
     </View>
   );
 }
