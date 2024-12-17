77a7a4f8caee94f9392e2bea90a6f7a83ff6df1e
javel <javelawilson@gmail.com>
2024-12-17 03:17:26 -0500

    implement family plans page

--- src/app/(app)/profile/family-plans.tsx
+++ src/app/(app)/profile/family-plans.tsx
@@ -1,9 +1,35 @@
-import { Text, View } from "react-native";
+import { PrivateProfile } from "@/api/my-profile/types";
+import { useFamilyPlans } from "@/api/options";
+import { RadioList } from "@/components/radio-list";
+import { StackHeaderV4 } from "@/components/stack-header-v4";
+import { useEdit } from "@/store/edit";
+import { router } from "expo-router";
+import { useState } from "react";
+import { View } from "react-native";
 
 export default function Page() {
+  const { edits, setEdits } = useEdit();
+  const { data } = useFamilyPlans();
+  const [selected, setSelected] = useState(edits?.family_plan || null);
+
+  const handlePress = () => {
+    if (selected) {
+      setEdits({
+        ...edits,
+        family_plan: selected,
+      } as PrivateProfile);
+    }
+    router.back();
+  };
+
   return (
-    <View>
-      <Text>FamilyPlans</Text>
+    <View className="flex-1 bg-white p-5">
+      <StackHeaderV4 title="Family Plans" onPressBack={handlePress} />
+      <RadioList
+        options={data}
+        onChange={setSelected}
+        initialSelection={selected}
+      />
     </View>
   );
 }
