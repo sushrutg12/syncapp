e544eea1e391c0a7ede2e5678025ee51cf52a2e9
javel <javelawilson@gmail.com>
2024-12-17 03:09:03 -0500

    implement gender preferences page

--- src/app/(app)/profile/gender-preferences.tsx
+++ src/app/(app)/profile/gender-preferences.tsx
@@ -1,9 +1,38 @@
-import { Text, View } from "react-native";
+import { PrivateProfile } from "@/api/my-profile/types";
+import { useGenders } from "@/api/options";
+import { CheckboxList } from "@/components/checkbox-list";
+import { StackHeaderV4 } from "@/components/stack-header-v4";
+import { useEdit } from "@/store/edit";
+import { router } from "expo-router";
+import { useState } from "react";
+import { View } from "react-native";
 
 export default function Page() {
+  const { edits, setEdits } = useEdit();
+  const { data } = useGenders();
+  const [selected, setSelected] = useState(edits?.gender_preferences || []);
+
+  const handlePress = () => {
+    if (selected) {
+      setEdits({
+        ...edits,
+        gender_preferences: selected,
+      } as PrivateProfile);
+    }
+    router.back();
+  };
+
   return (
-    <View>
-      <Text>GenderPreferences</Text>
+    <View className="flex-1 bg-white p-5">
+      <StackHeaderV4 title="I'm interested in" onPressBack={handlePress} />
+      <CheckboxList
+        options={data.map((item) => ({
+          id: item.id,
+          name: item.plural_name || item.name,
+        }))}
+        onChange={setSelected}
+        initialSelection={selected}
+      />
     </View>
   );
 }
