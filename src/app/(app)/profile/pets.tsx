ff7586e8f0d76cd88924438b86a11eb61c47e9f2
javel <javelawilson@gmail.com>
2024-12-17 03:04:18 -0500

    implement pets page

--- src/app/(app)/profile/pets.tsx
+++ src/app/(app)/profile/pets.tsx
@@ -1,9 +1,35 @@
-import { Text, View } from "react-native";
+import { PrivateProfile } from "@/api/my-profile/types";
+import { usePets } from "@/api/options";
+import { CheckboxList } from "@/components/checkbox-list";
+import { StackHeaderV4 } from "@/components/stack-header-v4";
+import { useEdit } from "@/store/edit";
+import { router } from "expo-router";
+import { useState } from "react";
+import { View } from "react-native";
 
 export default function Page() {
+  const { edits, setEdits } = useEdit();
+  const { data } = usePets();
+  const [selected, setSelected] = useState(edits?.pets || []);
+
+  const handlePress = () => {
+    if (selected) {
+      setEdits({
+        ...edits,
+        pets: selected,
+      } as PrivateProfile);
+    }
+    router.back();
+  };
+
   return (
-    <View>
-      <Text>Pets</Text>
+    <View className="flex-1 bg-white p-5">
+      <StackHeaderV4 title="Pets" onPressBack={handlePress} />
+      <CheckboxList
+        options={data}
+        onChange={setSelected}
+        initialSelection={selected}
+      />
     </View>
   );
 }
