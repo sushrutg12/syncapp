9b895b9a6a2cc626f572aa5c8ecefc19ccda56d9
javel <javelawilson@gmail.com>
2024-12-17 03:05:25 -0500

    implement pronouns page

--- src/app/(app)/profile/pronouns.tsx
+++ src/app/(app)/profile/pronouns.tsx
@@ -1,9 +1,35 @@
-import { Text, View } from "react-native";
+import { PrivateProfile } from "@/api/my-profile/types";
+import { usePronouns } from "@/api/options";
+import { CheckboxList } from "@/components/checkbox-list";
+import { StackHeaderV4 } from "@/components/stack-header-v4";
+import { useEdit } from "@/store/edit";
+import { router } from "expo-router";
+import { useState } from "react";
+import { View } from "react-native";
 
 export default function Page() {
+  const { edits, setEdits } = useEdit();
+  const { data } = usePronouns();
+  const [selected, setSelected] = useState(edits?.pronouns || []);
+
+  const handlePress = () => {
+    if (selected) {
+      setEdits({
+        ...edits,
+        pronouns: selected,
+      } as PrivateProfile);
+    }
+    router.back();
+  };
+
   return (
-    <View>
-      <Text>Pronouns</Text>
+    <View className="flex-1 bg-white p-5">
+      <StackHeaderV4 title="Pronouns" onPressBack={handlePress} />
+      <CheckboxList
+        options={data}
+        onChange={setSelected}
+        initialSelection={selected}
+      />
     </View>
   );
 }
