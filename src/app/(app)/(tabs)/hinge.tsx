8d991cc494fe81267a269a72ebbe2b1f4c9a6a12
Mason Arditi <masonarditi@berkeley.edu>
2025-05-03 07:41:16 +0800

    fully updated settings page

--- src/app/(app)/(tabs)/hinge.tsx
+++ src/app/(app)/(tabs)/hinge.tsx
@@ -1,5 +1,4 @@
 import { useMyProfile } from "@/api/my-profile";
-import { Card } from "@/components/card";
 import { Ionicons } from "@expo/vector-icons";
 import { Image } from "expo-image";
 import { Link, router, Stack } from "expo-router";
@@ -8,6 +7,39 @@ import React from "react";
 import { Pressable, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import HingeLogo from "~/assets/images/hinge-logo.svg";
+
+// Create a custom card component right in the file
+const GoldCard = ({ icon, title, subtitle, ...rest }) => {
+  return (
+    <Pressable
+      {...rest}
+      className="flex-row items-center gap-4 border rounded-md p-3"
+      style={{ borderColor: "#ecac6d" }}
+    >
+      <View
+        className="h-12 aspect-square rounded-full items-center justify-center"
+        style={{
+          backgroundColor: "#1a1a1a",
+          borderColor: "#ecac6d",
+          borderWidth: 1,
+        }}
+      >
+        {icon}
+      </View>
+      <View>
+        <Text className="font-poppins-medium" style={{ color: "#ecac6d" }}>
+          {title}
+        </Text>
+        <Text
+          className="text-sm font-poppins-light"
+          style={{ color: "#ecac6d" }}
+        >
+          {subtitle}
+        </Text>
+      </View>
+    </Pressable>
+  );
+};
 
 export default function Page() {
   const { data: profile } = useMyProfile();
@@ -20,7 +52,10 @@ export default function Page() {
       <SafeAreaView edges={["top", "bottom"]} className="flex-1 bg-gray-900">
         <Stack.Screen options={{ headerShown: false }} />
 
-        <View className="px-5 border-b border-neutral-300">
+        <View
+          className="px-5 border-b border-neutral-300"
+          style={{ borderColor: "#ecac6d" }}
+        >
           <View className="flex-row items-center justify-between">
             <HingeLogo width={64} />
             <View className="flex-row items-center gap-4">
@@ -62,7 +97,7 @@ export default function Page() {
         </View>
 
         <View className="flex-1 p-5 gap-4">
-          <Card
+          <GoldCard
             key="help"
             icon={
               <Ionicons
@@ -74,7 +109,7 @@ export default function Page() {
             title="Help Center"
             subtitle="Safety, Security, and more"
           />
-          <Card
+          <GoldCard
             key="what-works"
             icon={
               <Ionicons
